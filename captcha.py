5ca643aa18d77a335eacabaa8e08b5f6f16a6858
zeraye <jakubrudnik02@gmail.com>
2021-03-22 22:23:59 +0100

    comments

--- captcha.py
+++ captcha.py
@@ -1,14 +1,17 @@
+# importing libraries
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import pyperclip
 from selenium.webdriver.common.keys import Keys
 import time
 
+# driver options
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://revcaptcha.com/')
 driver.implicitly_wait(20)
 
+# clicking first checkbox
 checkbox_btn = driver.find_element_by_class_name('revCaptchaCheckbox')
 checkbox_btn.click()
 
@@ -27,50 +30,18 @@ def solve(equation):
             return int(new_equation[0]) * int(new_equation[1])
     return ''
 
-
+# wait to load animation
 time.sleep(1)
 
+input = ''
 
-######## PIERWSZA WERSJA WOLNA ROBI OKOŁO 95 ########
-
-# i = 0
-# divs = driver.find_elements_by_class_name('revCaptchaEquationRow')
-# for div in divs:
-#     equation = div.get_attribute('innerHTML')
-#     equation = equation[7:].split('<')[0]
-#     answer = solve(equation)
-#     input_label = div.find_element_by_name('solution-'+str(i))
-#     i += 1
-#     print(i)
-#     pyperclip.copy(answer)
-#     input_label.send_keys(Keys.CONTROL,'v')
-
-
-######## JS SIĘ ZWALIŁO I COFA INPUT VALUE ########
-######## JS CAŁY CZAS ZWALONY ALE SZYBKOŚĆ JEST TURBO MOCNA WIĘC DZIAŁA ########
-
-input = ""
-
+# not best, but work okay?
 for i in range(500):
     input += f"""document.querySelector("input[name='solution-{i}']").value = eval(document.querySelectorAll("div[class='revCaptchaEquationRow'] label")[{i}].textContent);"""
 
 
 driver.execute_script(input)
 
+# clicking last button
 submit_btn = driver.find_element_by_class_name('revCaptchaSubmitButton')
 submit_btn.click()
-
-######## DZIAŁA ALE WOLNO OKOŁO 90 ROBI ########
-
-# inputs = driver.find_elements_by_tag_name('input')[:-1]
-# equations = driver.find_elements_by_class_name('revCaptchaEquationRow')
-
-# i = 0
-
-# for i in range(len(inputs)):
-#     equation = equations[i].text
-#     answer = solve(equation)
-#     pyperclip.copy(answer)
-#     inputs[i].send_keys(Keys.CONTROL,'v')
-#     print(i)
-    
